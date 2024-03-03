49efeb7da88e5c7ceb200f5c74f29d7132b63ea3
Michael <michael.burdge@hotmail.co.uk>
2024-03-03 17:58:39 +0000

    Changes up the starting place

---
+++ Commands/MyCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace ProperTabGroups
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            var variables = await VS.Windows.GetAllWindowsAsync();
+
+            if (variables != null)
+            {
+                foreach (var window in variables)
+                {
+                }
+            }
+
+        }
+    }
+}
