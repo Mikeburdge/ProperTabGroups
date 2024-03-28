c03375fabc5d4dad9a24549de961e74d149756eb
Michael Burdge <Michael.burdge@hotmail.co.uk>
2024-03-28 17:45:06 +0000

    Possibly going to revert this NewContextMenuStuff
    Zach showed me a different way of doing it and im not sure if it's going to work

--- ToolWindows/ProperTabGroupsWindowControl.xaml.cs
+++ ToolWindows/ProperTabGroupsWindowControl.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Media;
 using System.Windows;
 using System.Windows.Input;
 using ProperTabGroups.TabGroupScripts;
+using MessageBox = System.Windows.MessageBox;
 using SelectionChangedEventArgs = System.Windows.Controls.SelectionChangedEventArgs;
 using TabInfo = ProperTabGroups.TabGroupScripts.TabInfo;
 using Window = EnvDTE.Window;
@@ -21,7 +22,6 @@ namespace ProperTabGroups
 
         private bool _bIsSelectionChangeProgrammatic;
 
-        private TabInfo _filterModificationCurrentTabInfo = null;
 
         public ProperTabGroupsSubsystem ViewModel => ProperTabGroupsSubsystem.Instance;
 
@@ -192,12 +192,6 @@ namespace ProperTabGroups
             ViewModel.CreateNewTabGroup(userInput);
         }
 
-        private void TabGroupsListView_OnMouseRightButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            ContextMenu cm = FindResource("TabInfoContextMenu") as ContextMenu;
-            cm.PlacementTarget = sender as Button;
-            cm.IsOpen = true;
-        }
         private object FindDataContextForFrameworkElement(FrameworkElement element)
         {
             if (element.DataContext != null)
@@ -218,42 +212,6 @@ namespace ProperTabGroups
             return null;
         }
 
-        private void AddFilter_OnClick(object sender, RoutedEventArgs e)
-        {
-            TabInfo selectedTabInfo = GetClickedTabInfo(sender);
-
-            if (selectedTabInfo == null) return;
-
-            // Storing this to use in the next window (@see AddFiltersList_OnSelectionChanged)
-            _filterModificationCurrentTabInfo = selectedTabInfo;
-
-            // Proceed to remove the filter from tabInfo
-            List<string> itemSource = GetAddFilterItemSource(selectedTabInfo);
-
-            if (!itemSource.Any()) return;
-
-            AddFiltersList.ItemsSource = itemSource;
-            ListOfAddFiltersPopup.IsOpen = true;
-        }
-
-        private void RemoveFilter_OnClick(object sender, RoutedEventArgs e)
-        {
-            TabInfo selectedTabInfo = GetClickedTabInfo(sender);
-
-            if (selectedTabInfo == null) return;
-
-            // Storing this to use in the next window (@see RemoveFiltersList_OnSelectionChanged)
-            _filterModificationCurrentTabInfo = selectedTabInfo;
-
-            // Proceed to remove the filter from tabInfo
-            List<string> itemSource = RetrievePurifiedItemSource(selectedTabInfo);
-
-            if (!itemSource.Any()) return;
-
-            RemoveFiltersList.ItemsSource = itemSource;
-
-            ListOfRemoveFiltersPopup.IsOpen = true;
-        }
         private TabInfo GetClickedTabInfo(object sender)
         {
             MenuItem menuItem = sender as MenuItem;
@@ -265,71 +223,208 @@ namespace ProperTabGroups
             return selectedTabInfo;
         }
 
-        private void AddFiltersList_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (AddFiltersList.SelectedItem is not string filter) return;
+        //private void AddFilter_OnClick(object sender, RoutedEventArgs e)
+        //{
+        //    TabInfo selectedTabInfo = GetClickedTabInfo(sender);
 
-            TabInfo selectedTabInfo = _filterModificationCurrentTabInfo;
-            if (selectedTabInfo == null) return;
+        //    if (selectedTabInfo == null) return;
 
-            ProperTabGroupsSubsystem.AddFilterToTab(selectedTabInfo, filter);
+        //    // Storing this to use in the next window (@see AddFiltersList_OnSelectionChanged)
+        //    _filterModificationCurrentTabInfo = selectedTabInfo;
 
-            AddFiltersList.SelectedItem = null;
+        //    // Proceed to remove the filter from tabInfo
+        //    List<string> itemSource = GetAddFilterItemSource(selectedTabInfo);
 
-            List<string> itemSource = GetAddFilterItemSource(selectedTabInfo);
+        //    if (!itemSource.Any()) return;
 
-            if (itemSource != null && itemSource.Any())
-            {
-                AddFiltersList.ItemsSource = itemSource;
-            }
-            else
-            {
-                // Get rid of it when we are done
-                _filterModificationCurrentTabInfo = null;
-                ListOfAddFiltersPopup.IsOpen = false;
-            }
-        }
+        //    AddFiltersList.ItemsSource = itemSource;
+        //    ListOfAddFiltersPopup.IsOpen = true;
+        //}
 
-        private void RemoveFiltersList_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (RemoveFiltersList.SelectedItem is not string filter) return;
+        //private void RemoveFilter_OnClick(object sender, RoutedEventArgs e)
+        //{
+        //    TabInfo selectedTabInfo = GetClickedTabInfo(sender);
 
-            TabInfo selectedTabInfo = _filterModificationCurrentTabInfo;
-            if (selectedTabInfo == null) return;
+        //    if (selectedTabInfo == null) return;
 
-            ProperTabGroupsSubsystem.RemoveFilterFromTab(selectedTabInfo, filter);
+        //    // Storing this to use in the next window (@see RemoveFiltersList_OnSelectionChanged)
+        //    _filterModificationCurrentTabInfo = selectedTabInfo;
 
-            RemoveFiltersList.SelectedItem = null;
+        //    // Proceed to remove the filter from tabInfo
+        //    List<string> itemSource = GetRemoveFilterItemSource(selectedTabInfo);
 
-            List<string> itemSource = RetrievePurifiedItemSource(selectedTabInfo);
+        //    if (!itemSource.Any()) return;
 
-            if (itemSource.Any())
-            {
-                RemoveFiltersList.ItemsSource = itemSource;
-            }
-            else
-            {
-                // Get rid of it when we are done
-                _filterModificationCurrentTabInfo = null;
-                ListOfRemoveFiltersPopup.IsOpen = false;
-            }
-        }
+        //    RemoveFiltersList.ItemsSource = itemSource;
+
+        //    ListOfRemoveFiltersPopup.IsOpen = true;
+        //}
+
+        //private void AddFiltersList_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    if (AddFiltersList.SelectedItem is not string filter) return;
+
+        //    TabInfo selectedTabInfo = _filterModificationCurrentTabInfo;
+        //    if (selectedTabInfo == null) return;
+
+        //    ProperTabGroupsSubsystem.AddFilterToTab(selectedTabInfo, filter);
+
+        //    AddFiltersList.SelectedItem = null;
+
+        //    List<string> itemSource = GetAddFilterItemSource(selectedTabInfo);
+
+        //    if (itemSource != null && itemSource.Any())
+        //    {
+        //        AddFiltersList.ItemsSource = itemSource;
+        //    }
+        //    else
+        //    {
+        //        // Get rid of it when we are done
+        //        _filterModificationCurrentTabInfo = null;
+        //        ListOfAddFiltersPopup.IsOpen = false;
+        //    }
+        //}
+
+        //private void RemoveFiltersList_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    if (RemoveFiltersList.SelectedItem is not string filter) return;
+
+        //    TabInfo selectedTabInfo = _filterModificationCurrentTabInfo;
+        //    if (selectedTabInfo == null) return;
+
+        //    ProperTabGroupsSubsystem.RemoveFilterFromTab(selectedTabInfo, filter);
+
+        //    RemoveFiltersList.SelectedItem = null;
+
+        //    List<string> itemSource = GetRemoveFilterItemSource(selectedTabInfo);
+
+        //    if (itemSource.Any())
+        //    {
+        //        RemoveFiltersList.ItemsSource = itemSource;
+        //    }
+        //    else
+        //    {
+        //        // Get rid of it when we are done
+        //        _filterModificationCurrentTabInfo = null;
+        //        ListOfRemoveFiltersPopup.IsOpen = false;
+        //    }
+        //}
 
         private List<string> GetAddFilterItemSource(TabInfo selectedTabInfo)
         {
             List<string> itemSource = ViewModel.GetAvailableTabGroupNames(selectedTabInfo).ToList();
-            // Remove from unassigned if it's a part of this group
+
             itemSource.Remove(ProperTabGroupsSubsystem.UnassignedTabsGroupName);
             return itemSource;
         }
 
-        private List<string> RetrievePurifiedItemSource(TabInfo selectedTabInfo)
+        private List<string> GetRemoveFilterItemSource(TabInfo selectedTabInfo)
         {
-            // Remove from unassigned if it's a part of this group
             List<string> itemSource = selectedTabInfo.Filters.ToList();
 
             itemSource.Remove(ProperTabGroupsSubsystem.UnassignedTabsGroupName);
             return itemSource;
+        }
+
+        private void UIElement_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            TabInfo selectedTabInfo = GetClickedTabInfo(sender);
+
+            if (selectedTabInfo == null) return;
+
+
+            if (sender is not MenuItem menu) return;
+
+            List<string> itemSource;
+
+            switch (menu.Name)
+            {
+                case "AddMenuItem":
+                    {
+                        itemSource = GetAddFilterItemSource(selectedTabInfo);
+                        break;
+                    }
+                case "RemoveMenuItem":
+                    {
+                        itemSource = GetRemoveFilterItemSource(selectedTabInfo);
+                        break;
+                    }
+                default:
+                {
+                    // Ensure we initialise it
+                    itemSource = [];
+                    break;
+                }
+            }
+
+            menu.Items.Clear();
+            if (!itemSource.Any()) return;
+
+            foreach (string item in itemSource)
+            {
+                MenuItem child = new MenuItem();
+                child.Header = item;
+                child.Click += Child_Click;
+
+                menu?.Items.Add(child);
+            }
+        }
+
+        private void Child_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("HELLO");
+        }
+
+        private void TabInfoContextMenu_OnContextMenuOpening(object sender, ContextMenuEventArgs e)
+        {
+            if (sender is not ContextMenu menu) return;
+
+            MenuItem addMenuItem = menu.Items.OfType<MenuItem>().FirstOrDefault(x => x.Name == "AddMenuItem");
+
+
+            TabInfo selectedTabInfo = GetClickedTabInfo(addMenuItem);
+
+            if (selectedTabInfo == null) return;
+
+
+            // Proceed to remove the filter from tabInfo
+            List<string> itemSource = GetAddFilterItemSource(selectedTabInfo);
+
+            addMenuItem.Items.Clear();
+            if (!itemSource.Any()) return;
+
+            foreach (string item in itemSource)
+            {
+                MenuItem child = new MenuItem();
+                child.Header = item;
+                child.Click += Child_Click;
+
+                addMenuItem.Items.Add(child);
+            }
+
+
+            MenuItem removeMenuItem = menu.Items.OfType<MenuItem>().FirstOrDefault(x => x.Name == "RemoveMenuItem");
+
+
+            selectedTabInfo = GetClickedTabInfo(removeMenuItem);
+
+            if (selectedTabInfo == null) return;
+
+
+            // Proceed to remove the filter from tabInfo
+            itemSource = GetRemoveFilterItemSource(selectedTabInfo);
+
+            removeMenuItem.Items.Clear();
+            if (!itemSource.Any()) return;
+
+            foreach (string item in itemSource)
+            {
+                MenuItem child = new MenuItem();
+                child.Header = item;
+                child.Click += Child_Click;
+
+                removeMenuItem.Items.Add(child);
+            }
         }
     }
 }
