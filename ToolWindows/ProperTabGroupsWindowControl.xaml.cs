47346d43f3905e1c706c1677198a84b707b87e7d
Michael HomePC <Michael.burdge@hotmail.co.uk>
2024-06-01 02:12:36 +0100

    its something

--- ToolWindows/ProperTabGroupsWindowControl.xaml.cs
+++ ToolWindows/ProperTabGroupsWindowControl.xaml.cs
@@ -232,10 +232,5 @@ namespace ProperTabGroups
             searchTextBox.Foreground = Brushes.Gray;
             searchTextBox.Text = "Search...";
         }
-
-        private void Expander_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-
-        }
     }
 }
